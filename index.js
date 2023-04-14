bf6039e4326e70a924e6188a664779d4af75c9cc
huzaifa3165 <83898816+huzaifa3165@users.noreply.github.com>
2023-04-14 10:22:05 +0500

    build optimization

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const request = require("request");
 const { json } = require("body-parser");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
@@ -9,52 +10,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
-})
+  res.sendFile(__dirname + "/index.html");
+});
 app.post("/", function (req, res) {
-    let first_name = req.body.fname
-    let last_name = req.body.lname
-    let emailAddress = req.body.email
-    let data = {
-        members: [
-            {
-                email_address: emailAddress,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: first_name,
-                    LNAME: last_name
-                }
-            }
-        ]
-    }
-    let JSONData = JSON.stringify(data);
-    let options = {
-        url: "https://us9.api.mailchimp.com/3.0/lists/f3a793a1dc",
-        method: "POST",
-        headers: {
-            "Authorization": "huzaifa3165 8c92ba8fa759449104ad3294bccf7beb-us9"
+  let first_name = req.body.fname;
+  let last_name = req.body.lname;
+  let emailAddress = req.body.email;
+  let data = {
+    members: [
+      {
+        email_address: emailAddress,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: first_name,
+          LNAME: last_name,
         },
-        body: JSONData
+      },
+    ],
+  };
+  let JSONData = JSON.stringify(data);
+  console.log(process.env.API_URL);
+  let options = {
+    url: process.env.API_URL,
+    method: "POST",
+    headers: {
+      Authorization: process.env.AUTH_TOKEN,
+    },
+    body: JSONData,
+  };
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
     }
-    request(options, function (error, response, body) {
-        if (error) {
-            res.sendFile(__dirname + "/failure.html")
-        } else {
-            if (response.statusCode == 200) {
-                res.sendFile(__dirname + "/success.html")
-            }
-            else {
-                res.sendFile(__dirname + "/failure.html")
-            }
-        }
-    })
-})
-
+  });
+});
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log(`Server listening at port ` + port + `
-Go to localhost:` + port)
-})
+  console.log(
+    `Server listening at port ` +
+      port +
+      `
+Go to localhost:` +
+      port
+  );
+});
 
 // List ID:   f3a793a1dc
 
